e9b3c865bac185eb8d1eb3b9e94a31ce3ce288a8
hansow <nachoagus@live.com.ar>
2018-10-31 23:19:23 -0300

    Add files via upload
    Las naves enemigas ya pueden agarrar y capturar la bandera

---
+++ Scripts/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    List<GameObject> naves;
+    EnemyController enemy;
+
+    public void Start()
+    {
+        naves = new List<GameObject>();
+        enemy = GetComponentInChildren<EnemyController>();
+
+        foreach (Transform t in transform)
+        {
+            naves.Add(t.gameObject);
+        }
+        naves[0].GetComponent<EnemyController>().isInChargeOfTakingFlag = true;
+        
+    }
+   /* public void Update()
+    {
+        naves[0].GetComponent<EnemyController>().isGoingToFlag = true;
+           
+                int rdm = Random.Range(0, naves.Count);
+                naves[rdm].GetComponent<EnemyController>().CapturarBandera();
+    }*/
+
+    }
---
+++ Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * speed;
+    }
+
+}
